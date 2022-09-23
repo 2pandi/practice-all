295931300cc3e3c2f590a7bec547e080d8e90914
2pandi <coder.2pandi@gmail.com>
2022-09-23 09:50:43 +0900

    :sparkles: feat: Initialize firebase

--- 4. Nomad Coders/2witter/src/components/App.js
+++ 4. Nomad Coders/2witter/src/components/App.js
@@ -1,11 +1,13 @@
 import AppRouter from "components/Router";
 import { useState } from "react";
+import { authService } from "fbase";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn ] = useState(false);
+  const [isLoggedIn, setIsLoggedIn ] = useState(authService.currentUser);
   return (
     <AppRouter isLoggedIn={isLoggedIn} />
   );
 }
 
 export default App;
+
