b5da9ff1d9d3f26a4eccd5d1c830959d3d3c43f3
2pandi <coder.2pandi@gmail.com>
2022-09-25 22:36:54 +0900

    :construction: feat: Set Auth component

--- 4. Nomad Coders/2witter/src/routes/Auth.js
+++ 4. Nomad Coders/2witter/src/routes/Auth.js
@@ -1,8 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const onChange = (e) => {
+      const { name, value } = e.target;
+      if (name === "email") {
+        setEmail(value);
+      } else if (name === "password") {
+        setPassword(value);
+      }
+    };
+    const onSubmit = (e) => {
+      e.preventDefault();
+    };
+
+    /**
+     * const [form, setForm] = useState({email: "", password: ""});
+     * const onChange = ({target: {name, value}}) => setForm({...form, [name]: value});
+     */
+
     return (
-        <span>Auth</span>
+      <span>
+        <form onSubmit={onSubmit}>
+          <input
+            name="email"
+            type="email"
+            placeholder="Email"
+            required
+            value={email}
+            onChange={onChange}
+          />
+          <input
+            name="password"
+            type="password"
+            placeholder="Password"
+            required
+            value={password}
+            onChange={onChange}
+          />
+          <input type="submit" value="Log in"/>
+        </form>
+        <div>
+          <button>Countinue with Google</button>
+          <button>Countinue with Github</button>
+        </div>
+      </span>
     );
 }
 export default Auth;
