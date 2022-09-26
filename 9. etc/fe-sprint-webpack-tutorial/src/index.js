ef9f268c99fd031a84dcd3a49bbc8a472093945c
2pandi <coder.2pandi@gmail.com>
2022-09-26 15:22:33 +0900

    :construction: practice: tutorial 2

--- 9. etc/fe-sprint-webpack-tutorial/src/index.js
+++ 9. etc/fe-sprint-webpack-tutorial/src/index.js
@@ -1,12 +1,29 @@
 const _ = require('./underbar')
+require('./style.css');
 
 const shout = (...sentences) => console.log(...sentences);
+const shoutToHTML = (...sentences) => {
+    const app = document.querySelector('#app');
+    app.append(...sentences.map(sentence =>{
+        const shoutHere = document.createElement('div');
+        shoutHere.className = 'shout';
+        shoutHere.textContent = sentence;
+        return shoutHere;
+    }))
+    return;
+};
 
 const loremIpsum = "Aliqua esse voluptate pariatur incididunt sunt eiusmod."
 
 const shoutOnce = _.once(shout);
+const shoutToHTMLOnce = _.once(shoutToHTML);
 
 shoutOnce(loremIpsum);
 shoutOnce(loremIpsum);
 shoutOnce(loremIpsum);
 shoutOnce(loremIpsum);
+
+shoutToHTMLOnce(loremIpsum);
+shoutToHTMLOnce(loremIpsum);
+shoutToHTMLOnce(loremIpsum);
+shoutToHTMLOnce(loremIpsum);
--- 9. etc/fe-sprint-webpack-tutorial/webpack.config.js
+++ 9. etc/fe-sprint-webpack-tutorial/webpack.config.js
@@ -6,4 +6,13 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'app.bundle.js',
     },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+                exclude: /node_modules/,
+            },
+        ],
+    },
 };
