afd3a52d748c5ce9face278b38c8dd835c60263b
2pandi <coder.2pandi@gmail.com>
2022-09-27 16:21:33 +0900

    :construction: feat: Add twoweet in Home

--- 4. Nomad Coders/2witter/src/routes/Home.js
+++ 4. Nomad Coders/2witter/src/routes/Home.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Home = () => {
+    const [twoweet, setTwoweet] = useState('');
+    const onSubmit = (e) => {
+        e.preventDefault();
+    };
+    const onChange = (e) => {
+        setTwoweet(e.target.value);
+    };
     return (
-        <span>Home</span>
+        <form onSubmit={onSubmit}>
+            <input onChange={onChange} value={twoweet} type="text" placeholder="What's on your mind?" maxLength={120} />
+            <input type="submit" value="2witter" />
+        </form>
     );
 }
 export default Home;
