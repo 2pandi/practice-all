4fa5dd0558fd20a21ea0baa34324d98eaa6b007c
2pandi <coder.2pandi@gmail.com>
2022-09-27 16:06:58 +0900

    :sparkles: feat: Add Log out feature

--- 4. Nomad Coders/2witter/src/components/Router.js
+++ 4. Nomad Coders/2witter/src/components/Router.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
 import Navigation from "components/Navigation";
@@ -15,14 +15,17 @@ function AppRouter ({isLoggedIn}) {
               <Route exact path="/">
                 <Home />
               </Route>
-              <Route exact path="/profile" >
+              <Route exact path="/profile">
                 <Profile isLoggedIn={isLoggedIn} />
               </Route>
             </>
           ) : (
-            <Route exact path="/">
-              <Auth />
-            </Route>
+            <>
+              <Route exact path="/">
+                <Auth />
+              </Route>
+              <Redirect to="/" />
+            </>
           )}
         </Switch>
       </Router>
--- 4. Nomad Coders/2witter/src/routes/Profile.js
+++ 4. Nomad Coders/2witter/src/routes/Profile.js
@@ -1,22 +1,11 @@
 import React from "react";
 import { authService } from "fbase";
-import { Redirect } from "react-router-dom";
 
 export const Profile = ({isLoggedIn}) => {
     const onLogOutClick = () => {
         authService.signOut();
     }
 
-    console.log(isLoggedIn);
-
-    return (
-      <>
-        {isLoggedIn ? (
-          <button onClick={onLogOutClick}>Log Out</button>
-        ) : (
-          <Redirect to="/" />
-        )}
-      </>
-    );
+    return <button onClick={onLogOutClick}>Log Out</button>;
 }
 export default Profile;
