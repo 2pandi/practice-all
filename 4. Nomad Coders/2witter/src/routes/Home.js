e88062d285db04ddb2d54de30c0cd8beb3d69692
2pandi <coder.2pandi@gmail.com>
2022-09-27 20:03:27 +0900

    :sparkles: feat: Create 2weets

--- 4. Nomad Coders/2witter/src/routes/Home.js
+++ 4. Nomad Coders/2witter/src/routes/Home.js
@@ -1,18 +1,59 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { dbService, authService } from "fbase";
+import { addDoc, getDocs, collection } from "firebase/firestore";
 
 export const Home = () => {
     const [twoweet, setTwoweet] = useState('');
-    const onSubmit = (e) => {
-        e.preventDefault();
+    const [twoweets, setTwoweets] = useState([]);
+    
+    const get2weets = async () => {
+        const db2weets = await getDocs(collection(dbService, "twoweets"));
+        db2weets.forEach(doc => {
+            const twoweetObject = {
+                ...doc.data(),
+                id: doc.id,
+            };
+            setTwoweets(prev => [twoweetObject, ...prev]);
+        })
     };
+
+    useEffect(() => {
+        get2weets();
+    }, [])
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        const userEmail = authService.currentUser.email;
+        await addDoc(collection(dbService, "twoweets"), {
+            userEmail,
+            twoweet,
+            createdAt: new Date().toLocaleString()
+        });
+    };
+
     const onChange = (e) => {
-        setTwoweet(e.target.value);
+        setTwoweet(e.target.value); 
     };
     return (
-        <form onSubmit={onSubmit}>
-            <input onChange={onChange} value={twoweet} type="text" placeholder="What's on your mind?" maxLength={120} />
-            <input type="submit" value="2witter" />
-        </form>
+        <>
+            <form onSubmit={onSubmit}>
+                <input
+                    onChange={onChange}
+                    value={twoweet}
+                    type="text"
+                    placeholder="What's on your mind?"
+                    maxLength={120}
+                />
+                <input type="submit" value="2witter" />
+            </form>
+            <section>
+                {twoweets.map(twoweet => <div key={twoweet.id}>
+                    <h4>{twoweet.userEmail}</h4>
+                    <div>{twoweet.createdAt}</div>
+                    <div>{twoweet.twoweet}</div>
+                </div>)}
+            </section>
+        </>
     );
 }
 export default Home;
